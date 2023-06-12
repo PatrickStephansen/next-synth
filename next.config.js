8e93e73e59d5e3e210cd2e8aa5f0cd59e316f21e
Patrick Stephansen <patrick.stephansen@standardbank.co.za>
2023-06-12 19:13:12 +0200

    use wasm envelope generator worklet

--- next.config.js
+++ next.config.js
@@ -1,4 +1,33 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, _) => {
+    config.plugins = [
+      ...config.plugins,
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: "node_modules/reactive-synth-bitcrusher/bitcrusher.js",
+            to: "../public/worklets",
+          },
+          {
+            from: "node_modules/reactive-synth-bitcrusher/reactive_synth_bitcrusher.wasm",
+            to: "../public/worklets",
+          },
+          {
+            from: "node_modules/reactive-synth-envelope-generator/envelope-generator.js",
+            to: "../public/worklets",
+          },
+          {
+            from: "node_modules/reactive-synth-envelope-generator/reactive_synth_envelope_generator.wasm",
+            to: "../public/worklets",
+          },
+        ],
+      }),
+    ];
+    return config;
+  },
+};
+
+module.exports = nextConfig;
--- src/lib/signal-chain.tsx
+++ src/lib/signal-chain.tsx
@@ -13,22 +13,63 @@ const audioContext = new AudioContext();
 interface Voice {
   oscillator: OscillatorNode;
   gain: GainNode;
+  envelopeGeneratorNode: AudioWorkletNode;
   isBusy: boolean;
   lastInvocationTime: number;
   note?: number;
 }
 
+const createModuleReadyPromise = (port: MessagePort) =>
+  new Promise<void>((resolve) =>
+    port.addEventListener("message", function moduleReady(event) {
+      if (
+        event.data &&
+        event.data.type === "module-ready" &&
+        event.data.value
+      ) {
+        resolve();
+        port.removeEventListener("message", moduleReady);
+      }
+    })
+  );
+
 export const initializeSignalChain = async () => {
   await audioContext.suspend();
+  await audioContext.audioWorklet.addModule("/worklets/envelope-generator.js");
+  const envelopeGeneratorBinaryResponse = await fetch(
+    "/worklets/reactive_synth_envelope_generator.wasm"
+  );
+  const envelopeGeneratorBinary =
+    await envelopeGeneratorBinaryResponse.arrayBuffer();
   for (let index = 0; index < OSCILLATOR_POOL_SIZE; index++) {
     const oscillator = new OscillatorNode(audioContext);
     const gain = new GainNode(audioContext, { gain: 0 });
+    const envelopeGeneratorNode = new AudioWorkletNode(
+      audioContext,
+      "reactive-synth-envelope-generator",
+      {
+        numberOfInputs: 0,
+        numberOfOutputs: 1,
+        channelCount: 1,
+        channelCountMode: "explicit",
+        outputChannelCount: [1],
+        processorOptions: { sampleRate: audioContext.sampleRate },
+      }
+    );
+    envelopeGeneratorNode.port.postMessage({
+      type: "wasm",
+      wasmModule: envelopeGeneratorBinary,
+    });
+    envelopeGeneratorNode.port.start();
+    await createModuleReadyPromise(envelopeGeneratorNode.port);
     oscillator.connect(gain);
     gain.connect(audioContext.destination);
+    envelopeGeneratorNode.connect(gain.gain);
     oscillator.start();
     oscillatorPool.push({
       oscillator,
       gain,
+      envelopeGeneratorNode,
       isBusy: false,
       lastInvocationTime: 0,
     });
@@ -58,7 +99,15 @@ export const handleMidiEvent = (
       keyNumberToOffsetInCents(eventData?.keyNumber ?? 48),
       0
     );
-    firstFreeOscillator.gain.gain.setValueAtTime(eventData.velocity, 0);
+    firstFreeOscillator.envelopeGeneratorNode.parameters
+      .get("trigger")
+      ?.setValueAtTime(eventData.velocity, 0);
+    firstFreeOscillator.envelopeGeneratorNode.parameters
+      .get("attackValue")
+      ?.setValueAtTime(eventData.velocity, 0);
+    firstFreeOscillator.envelopeGeneratorNode.parameters
+      .get("sustainValue")
+      ?.setValueAtTime(eventData.velocity / 4, 0);
   }
   if (eventData.eventType === "noteOff") {
     const ringingNote = oscillatorPool
@@ -66,7 +115,9 @@ export const handleMidiEvent = (
       .find((o) => o.note == eventData.keyNumber);
     if (ringingNote) {
       ringingNote.isBusy = false;
-      ringingNote.gain.gain.setValueAtTime(0, 0);
+      ringingNote.envelopeGeneratorNode.parameters
+        .get("trigger")
+        ?.setValueAtTime(0, 0);
     }
   }
 };
